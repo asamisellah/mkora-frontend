5611bf6ee1fc77c0adfbf13ce7568dd6a063867d
asamisellah <asami.rachael@gmail.com>
2023-08-04 18:44:12 +0300

    fix form issues

--- app/report/page.jsx
+++ app/report/page.jsx
@@ -5,75 +5,42 @@ import { useEffect, useState } from "react"
 import { useForm, ErrorMessage } from "react-hook-form"
 import Input from "../components/input"
 import Select from "../components/select"
-import { fields, options } from "../constants"
-// import { getOptions } from "../api/route"
-import useSWR from 'swr'
-import { getOptions } from "../api/route"
+import { fields } from "../constants"
+import { getOptions, postReport } from "../api/route"
+import { useRouter } from "next/navigation"
 
 
 const Report = () => {
-  // const [options, setOptions] = useState({})
-  const {data, error} = useSWR('https://mkora-service.azurewebsites.net/api/v1/options', getOptions)
+  const [options, setOptions] = useState({})
+  const router = useRouter()
 
   const { register, handleSubmit, formState: { errors, isSubmitting, isSubmitted }, } = useForm()
-  const language = "swahili"
+
+  useEffect(() => {
+    const fetchOptions = async () => {
+      const options = await getOptions()
+      console.log("Options", options)
+      setOptions(options)
+    }
+    fetchOptions()
+  }, [])
 
 
-  console.log("Options data", data)
 
-  // useEffect(() => {
-  //   const getOptions = async () => {
-  //     const response = await fetch("https://mkora-service.azurewebsites.net/api/v1/options", {
-  //       headers: {
-  //           "Referrer-Policy": "Access-Control-Allow-Origin"
-  //       }
-  //     })
-  //     const options = await response.json()
-  //     console.log("Options***********", options)
-  //   }
-  //   getOptions()   
-  // }, [])
 
-  // On submit:
-  // Check for valid state/errors (Validations if field is missing)
-  // Submit data
-  // Handle is submitting state
-  // Handle is submitted state i.e. both error and success
-
-  // useEffect(() => {
-  // await fetch(`http://localhost:9080/api/v1/options`, {
-  //   headers: {
-  //     'Content-Type': 'application/json',
-  //   }
-  //    const fetchOptions = async() => {
-  //     const result = await getOptions()
-  //     if (!ignore) {
-  //       console.log(result)
-  //       setOptions(result)
-  //     }
-  //   }
-
-  //   let ignore = false;
-
-  //   fetchOptions()
+  const onSubmit = async (data) => {    
+    const response = await postReport(data)
+    switch (response) {
+      case 'Success':
+        router.push('/analytics')
+        break
+      default:
+        break
       
-  //   return () => {
-  //     ignore = true;
-  //   };
-  // }, []) 
-
-  
-  
-
-  const onSubmit = (data) => {
-    console.log("*********", JSON.stringify(data))
-    console.log("@@@@@@@@@2", isSubmitted)
+    }
   }
 
-  const onError = () => {
-    console.log("########", errors)
-    console.log("@@@@@@@@@", isSubmitted)
-  }
+  const onError = () => {}
 
   const renderInputs = () => {
     return fields.map((field, index) => {
@@ -114,22 +81,16 @@ const Report = () => {
   
   return (
     <>
-      {/* {isSubmitted ? (
-        <div className="text-lightgray font-semibold text-xl text-center flex flex-col">
-          <p className="mb-4"> Successfully submitted. Thank you for your contribution! </p>
-          <Link className="text-primary mb-2 hover:text-opacity-75" href="/analytics">View statistics</Link>
-          <Link className="text-primary hover:text-opacity-75" href="/report">Back</Link>
-        </div>
-      ) : ( */}
-        <form className="flex flex-col" onSubmit={handleSubmit(onSubmit, onError)}>
-          {renderInputs()}
-          <input
-            className="submit-btn"
-            disabled={isSubmitting}
-            type="submit" />
-        </form>
-        {/* )
-      } */}
+      <form className="flex flex-col" onSubmit={handleSubmit(onSubmit, onError)}>
+        {renderInputs()}
+        {isSubmitting ? (<div className="submit-btn">sending...</div>)
+          : (
+            <input
+              className="submit-btn"
+              value="Report"
+              type="submit" />
+          )}
+      </form>
     </>
   )
 }
