173f150e4eead01a424929f109d3855502b7084e
asamisellah <asami.rachael@gmail.com>
2023-08-07 19:29:12 +0300

    make logo a link to home

--- app/components/brand.jsx
+++ app/components/brand.jsx
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import Link from 'next/link'
 import styles from "../styles/components/brand.css"
 
 // logo and name
@@ -6,7 +7,7 @@ export default function Brand({direction = "row"}) {
   return (
     <>
       { direction == "col" ? (
-        <div className='flex flex-col items-center'>
+        <Link href="/" className='flex flex-col items-center'>
           <Image
             src="/mkora.svg"
             alt="mkora-logo"
@@ -16,9 +17,9 @@ export default function Brand({direction = "row"}) {
           ></Image>
 
           <h1 className='text-primary sm:text-5xl md:text-7xl md:mt-6 font-semibold font-sans'>M-Kora</h1>
-        </div>
+        </Link>
       ) : (
-          <div className='flex flex-row items-center'>
+          <Link href="/" className='flex flex-row items-center'>
             <Image
               src="/mkora.svg"
               alt="mkora-logo"
@@ -28,7 +29,7 @@ export default function Brand({direction = "row"}) {
             ></Image>
 
             <h1 className='text-primary text-5xl font-semibold font-sans'>M-Kora</h1>
-          </div>
+          </Link>
           
       )}
     </>
